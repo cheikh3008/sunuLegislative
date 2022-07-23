504b629e47fdff19ef09004a63b03d0c8c1a7e78
beedigital2019 <beedigital2019@gmail.com>
2022-07-23 15:00:28 +0000

    up

--- src/Controller/RepresentantController.php
+++ src/Controller/RepresentantController.php
@@ -21,7 +21,9 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RepresentantController extends AbstractController
 {
@@ -56,12 +58,14 @@ class RepresentantController extends AbstractController
         foreach ($departements as $key => $value) {
             $ressultDepartements[$value->getNom()] = $value->getNom();
         }
+        $dt =  $this->session->get("circonscription", []);
         // dd($ressultDepartements);
         $form = $this->createFormBuilder()
             ->add('circonscription', ChoiceType::class, [
                 'label' => false,
                 'placeholder' => 'Choisir la circonscription',
                 'choices' => $ressultDepartements,
+                'data' => $dt ? $dt['circonscription'] : '',
                 // 'class' => Departement::class,
                 'constraints' => new NotBlank([
                     'message' => 'Veuillez choisir la circonscription .'
@@ -70,14 +74,15 @@ class RepresentantController extends AbstractController
             ->getForm();
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->session->get("circonscription", []);
+           
             $data = $form->getData();
             $this->session->set("circonscription", $data);
             // $this->addFlash('success', 'Le partenaire a été bien ajouté');
             return $this->redirectToRoute('app_commune_add', [], Response::HTTP_SEE_OTHER);
         }
         return $this->render('representant/add-circonscription.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'data' => $dt
         ]);
     }
 
@@ -91,11 +96,13 @@ class RepresentantController extends AbstractController
         foreach ($communes as $key => $value) {
             $com[$value->getCommune()] = $value->getCommune();
         }
+        $dt = $this->session->get("commune", []);
         $form = $this->createFormBuilder()
             ->add('commune', ChoiceType::class, [
                 'label' => false,
                 'placeholder' => 'Choisir la commune',
                 'choices' => $com,
+                'data' => $dt ? $dt['commune'] : '',
                 'constraints' => new NotBlank([
                     'message' => 'Veuillez choisir la commune .'
                 ]),
@@ -126,13 +133,15 @@ class RepresentantController extends AbstractController
         $bureauVote = $this->bureauVoteRepository->findBy(['commune' => $communes[0]]);
         // dd($bureauVote);
         foreach ($bureauVote as $key => $value) {
-            $bv[$value->getLieu() . ' - ' . $value->getNomBV()] = $value->getSlug();
+            $bv[$value->getLieu()] = $value->getLieu();
         }
+        $dt = $this->session->get("lieu");
         $form = $this->createFormBuilder()
-            ->add('commune', ChoiceType::class, [
+            ->add('lieu', ChoiceType::class, [
                 'label' => false,
                 'placeholder' => 'Choisir le lieu/ centre de vote',
                 'choices' => $bv,
+                'data' => $dt ? $dt['lieu'] : '',
                 'constraints' => new NotBlank([
                     'message' => 'Veuillez choisir le lieu/ centre de vote .'
                 ]),
@@ -145,15 +154,55 @@ class RepresentantController extends AbstractController
             $data = $form->getData();
             $bv = $this->bureauVoteRepository->findOneBy(['slug' => $data]);
             $userRS = $this->userRepository->findOneBy(['BV' => $bv]);
-            if ($userRS) {
-                $this->addFlash('error', "Ce bureau a été dèja affecté par un représentant");
-                return $this->redirectToRoute('app_lieu_add', [], Response::HTTP_SEE_OTHER);
-            }
+            // if ($userRS) {
+            //     $this->addFlash('error', "Ce bureau a été dèja affecté par un représentant");
+            //     return $this->redirectToRoute('app_lieu_add', [], Response::HTTP_SEE_OTHER);
+            // }
             $this->session->set("lieu", $data);
             // $this->addFlash('success', 'Le partenaire a été bien ajouté');
-            return $this->redirectToRoute('app_infos_add', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_bv_add', [], Response::HTTP_SEE_OTHER);
         }
         return $this->render('representant/add-lieu.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/representant/add-bv", name="app_bv_add")
+     */
+    public function addbv(Request $request): Response
+    {
+        $lieu = $this->session->get("lieu", []);
+        $bureauVote = $this->bureauVoteRepository->findBy(['lieu' => $lieu['lieu']]);
+        foreach ($bureauVote as $key => $value) {
+            $bv_nom[$value->getNomBV()] = $value->getSlug();
+        }
+        $dt = $this->session->get("nom_bv", []);
+        $form = $this->createFormBuilder()
+            ->add('nom_bv', ChoiceType::class, [
+                'label' => false,
+                'placeholder' => 'Choisir le numéro du bureau de vote',
+                'choices' => $bv_nom,
+                'data' => $dt ? $dt['nom_bv'] : '',
+                'constraints' => new NotBlank([
+                    'message' => 'Veuillez choisir le numéro du bureau de vote .'
+                ]),
+            ])
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $bv_res = $this->bureauVoteRepository->findOneBy(['slug' => $data['nom_bv']]);
+            $userRS = $this->userRepository->findOneBy(['BV' => $bv_res]);
+            if ($userRS) {
+                $this->addFlash('error', "Ce bureau a été dèja affecté par un représentant");
+                return $this->redirectToRoute('app_bv_add', [], Response::HTTP_SEE_OTHER);
+            }
+            
+            $this->session->set("nom_bv", $data);
+            return $this->redirectToRoute('app_infos_add', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->render('representant/bv.html.twig', [
             'form' => $form->createView()
         ]);
     }
@@ -165,17 +214,17 @@ class RepresentantController extends AbstractController
     {
         $user = new User();
         $role = $roleRepository->findOneBy(["libelle" => "ROLE_REPRESENTANT"]);
-        $slugBV = $this->session->get("lieu", []);
+        $slugBV = $this->session->get("nom_bv", []);
         $bureauVote = $this->bureauVoteRepository->findOneBy(['slug' => $slugBV]);
         // dd($bureauVote);
         $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         $password = substr(str_shuffle($chars), 0, 8);
-        $phoneUtil = PhoneNumberUtil::getInstance();
-        $codes_choice = [];
-        $regions = ($phoneUtil->getSupportedRegions());
-        foreach ($regions as  $value) {
-            $codes_choice[$value . " + " . $phoneUtil->getCountryCodeForRegion($value)] = $value;
-        }
+        // $phoneUtil = PhoneNumberUtil::getInstance();
+        // $codes_choice = [];
+        // $regions = ($phoneUtil->getSupportedRegions());
+        // foreach ($regions as  $value) {
+        //     $codes_choice[$value . " + " . $phoneUtil->getCountryCodeForRegion($value)] = $value;
+        // }
         $form = $this->createFormBuilder()
             ->add('nom', TextType::class, [
                 'label' => 'Nom',
@@ -195,12 +244,10 @@ class RepresentantController extends AbstractController
                     'message' => 'Veuillez remplir ce champs.'
                 ]),
             ])
-            ->add('code', ChoiceType::class, [
-                'label' => 'Code pays',
-                'choices'  =>  $codes_choice,
-                'placeholder' => 'Veuillez choisir un code pays',
+            ->add('check', CheckboxType::class, [
+                'label' => 'J\'accepte les termes et conditions',
                 'constraints' => new NotBlank([
-                    'message' => 'Veuillez remplir ce champs.'
+                    'message' => 'Veuillez accepter les termes et conditions.'
                 ]),
             ])
             ->add('telephone', NumberType::class, [
@@ -209,32 +256,37 @@ class RepresentantController extends AbstractController
                     'placeholder' => 'Entrez le numéro de téléphone',
                     'id' => 'phone'
                 ],
-                'constraints' => new NotBlank([
-                    'message' => 'Veuillez remplir ce champs.'
-                ]),
-                
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez remplir ce champs.'
+                    ]),
+                    new Regex([
+                        'pattern'  => '#^(77||78||76||70||75)[0-9]{9}$#',
+                        'message' => 'Veuillez entrer un numéro de téléphone valide.',
+                    ])
+                ],
+
             ])
             ->getForm();
         $form->handleRequest($request);
-
+        $usersAll = $this->userRepository->findAll();
         if ($form->isSubmitted() && $form->isValid()) {
             $telephone =  $form->get('telephone')->getData();
             $nom = $form->get('nom')->getData();
             $prenom = $form->get('prenom')->getData();
-            $code = $form->get('code')->getData();
-            $phone = $phoneUtil->parse($telephone, $code);
-            $number = $phone->getNationalNumber();
-            $indicatif = $phone->getCountryCode();
-            if (!$phoneUtil->isValidNumber($phone)) {
-                $this->addFlash('error', 'Le numéro de téléphone n\'est pas valide !');
-                return $this->redirectToRoute('app_infos_add', [], Response::HTTP_SEE_OTHER);
+    
+            foreach ($usersAll as $key => $value) {
+                if ($value->getTelephone() === (int)(221 . $telephone)) {
+                    $this->addFlash('error', 'ce numéro de téléphone existe dèja !');
+                    return $this->redirectToRoute('app_infos_add', [], Response::HTTP_SEE_OTHER);
+                }
             }
             $user->setRole($role);
             $user->setUuid($password);
-            $user->setCode($code);
+            $user->setCode("SN");
             $user->setNom($nom);
             $user->setPrenom($prenom);
-            $user->setTelephone(trim($indicatif . $telephone));
+            $user->setTelephone(trim(221 . $telephone));
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
@@ -244,14 +296,26 @@ class RepresentantController extends AbstractController
             $user->setBV($bureauVote);
             $user->setCommune($bureauVote->getCommune());
             $user->setLieu($bureauVote->getLieu());
-            $user->setUsername($number);
+            $user->setUsername($telephone);
             $em->persist($user);
             $em->flush();
             $this->addFlash('success', 'Ce représentant a été bien ajouté');
+            $this->session->remove("circonscription");
+            $this->session->remove("commune");
+            $this->session->remove("lieu");
+            $this->session->remove("nom_bv");
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
         return $this->render('representant/add-infos.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/representant/termes-conditions", name="app_termes_conditions")
+     */
+    public function app_termes_conditions(): Response
+    {
+        return $this->render('representant/termes-conditions.html.twig');
     }
 }
